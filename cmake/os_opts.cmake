f15afa04a80a40c826ccf8095d851923deaccdcf
rmdg88 <rmdg88@gmail.com>
2024-09-26 10:43:22 +0100

    try static pcre2

--- cmake/os_opts.cmake
+++ cmake/os_opts.cmake
@@ -4,7 +4,9 @@ message(STATUS "entering in 'os_opts.cmake'")
 
 if(WIN32)
    message(STATUS "compiling on windows")
-   set(LIB_LINK fmt json loguru cxxopts pcre2 sentencepiece sentencepiece_train fasttext utf8 z)
+   #set(LIB_LINK fmt json loguru cxxopts pcre2 sentencepiece sentencepiece_train fasttext utf8 z)
+   set(LIB_LINK fmt json loguru cxxopts pcre2 sentencepiece sentencepiece_train fasttext utf8 z ws2_32 user32)
+   add_definitions(-DPCRE2_STATIC)
 
 elseif(APPLE)
    message(STATUS "compiling on mac-osx")
